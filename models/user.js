6dd743dc64bff8ed2d3ce84991493904b597003f
Iyan Zuli Armanda <catcode0101@gmail.com>
2024-12-23 13:04:31 +0700

    add bootstrap offline file

---
+++ models/user.js
@@ -0,0 +1,5 @@
+// on going, middleware modul authentication, library passport
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const passportLocalMongoose = require('passport-local-mongoose')
